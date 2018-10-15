e8019f4185c7f911f622b68782dfd91ede692694
Takuro <fantasistakuro@gmail.com>
2018-10-16 00:57:32 +0900

    add line bot

--- run.py
+++ run.py
@@ -19,36 +19,39 @@ app = Flask(__name__)
 # 環境変数取得
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-# YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
-# YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
-#
-# line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-# handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-#
-#
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
-#
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-#
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-#
-#     return 'OK'
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
+YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
+
+LIFF_URL = "line://app/1614481927-DL6wVJEZ"
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text=event.message.text))
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.txt == "ほげ":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=LIFF_URL))
 
 
 # メッセージをランダムに表示するメソッド
