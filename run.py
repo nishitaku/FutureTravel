894990343c39da05752dff31c743c6c707ed9571
Takuro <fantasistakuro@gmail.com>
2018-10-16 00:20:57 +0900

    modify port

--- run.py
+++ run.py
@@ -90,5 +90,6 @@ def post():
 
 if __name__ == '__main__':
     app.debug = True  # デバッグモード有効化
-    app.run(host='0.0.0.0', port=3000) # どこからでもアクセス可能に
+    port = int(os.getenv('PORT', 3000))
+    app.run(host='0.0.0.0', port=port) # どこからでもアクセス可能に
     # app.run()
