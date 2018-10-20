6029915dc40d1943be4d865067108cb0d00ed80c
Takuro <fantasistakuro@gmail.com>
2018-10-21 04:33:25 +0900

    big fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,14 @@
+//var map;
+//var directions;
+rendererOptions = {
+    draggable: true,
+    preserveViewport: false
+};
+var directionsDisplay =
+    new google.maps.DirectionsRenderer(rendererOptions);
+var directionsService =
+    new google.maps.DirectionsService();
 var map;
-var directions;
 
 $(function () {
     //    initMap();
@@ -14,13 +23,51 @@ $('#mapping').click(() => {
 
 $(window).on('load', () => {
     console.log('onload')
-    if (GBrowserIsCompatible()) {
-        map = new GMap2(document.getElementById("map_canvas"));
-        map.setCenter(new GLatLng(35.681379, 139.765577), 13);
-
-        directions = new GDirections(map, document.getElementById('route'));
-    }
+    //    if (GBrowserIsCompatible()) {
+    //        map = new GMap2(document.getElementById("map_canvas"));
+    //        map.setCenter(new GLatLng(35.681379, 139.765577), 13);
+    //
+    //        directions = new GDirections(map, document.getElementById('route'));
+    //    }
+    initialize();
 })
+
+function initialize() {
+    var zoom = 7;
+    var mapTypeId = google.maps.MapTypeId.ROADMAP
+
+    var opts = {
+        zoom: zoom,
+        mapTypeId: mapTypeId
+    };
+    map = new google.maps.Map
+        (document.getElementById("map_canvas"), opts);
+    directionsDisplay.setMap(map);
+
+    google.maps.event.addListener(directionsDisplay,
+        'directions_changed', function () {
+        });
+    calcRoute("東京", "名古屋");
+}
+
+
+function calcRoute(src, dst) {
+    var request = {
+        origin: src,
+        destination: dst,
+        travelMode: google.maps.DirectionsTravelMode.DRIVING,
+        unitSystem: google.maps.DirectionsUnitSystem.METRIC,
+        optimizeWaypoints: true,
+        avoidHighways: false,
+        avoidTolls: false
+    };
+    directionsService.route(request,
+        function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
+}
 
 $(window).on('unload', () => {
     console.log('unload')
@@ -58,8 +105,9 @@ function dispRoute() {
     console.log(from)
     console.log(to)
 
-    directions.clear();
-
-    str = 'from: ' + from + ' to: ' + to;
-    directions.load(str, { locale: 'ja_JP' });
+//    directions.clear();
+//
+//    str = 'from: ' + from + ' to: ' + to;
+//    directions.load(str, { locale: 'ja_JP' });
+    calcRoute(from, to)
 }
