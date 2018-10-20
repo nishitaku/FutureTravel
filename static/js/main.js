46d5514840c7c6be3b651ba49a5683ae04a01d3d
Takuro <fantasistakuro@gmail.com>
2018-10-21 00:17:06 +0900

    fix bugs

--- static/js/main.js
+++ static/js/main.js
@@ -20,6 +20,12 @@ $(window).on('load', () => {
 
         directions = new GDirections(map, document.getElementById('route'));
     }
+
+    // LIFF
+    liff.init(function (data) {
+        alert(data)
+        initializeApp(data);
+    });
 })
 
 $(window).on('unload', () => {
@@ -62,4 +68,13 @@ function dispRoute() {
 
     str = 'from: ' + from + ' to: ' + to;
     directions.load(str, { locale: 'ja_JP' });
+}
+
+function initializeApp(data) {
+    document.getElementById('languagefield').textContent = data.language;
+    document.getElementById('viewtypefield').textContent = data.context.viewType;
+    document.getElementById('useridfield').textContent = data.context.userId;
+    document.getElementById('utouidfield').textContent = data.context.utouId;
+    document.getElementById('roomidfield').textContent = data.context.roomId;
+    document.getElementById('groupidfield').textContent = data.context.groupId;
 }
