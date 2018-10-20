c4c1624d2d7b43e795eba132480ebb97815ffafe
Takuro <fantasistakuro@gmail.com>
2018-10-21 04:12:34 +0900

    big fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,107 +1,65 @@
-//var map;
-//var directions;
-//
-//$(function () {
-//    //    initMap();
-//
-//
-//});
-//
-//$('#mapping').click(() => {
-//    //    renderMap();
-//    dispRoute();
-//});
+var map;
+var directions;
 
-//$(window).on('load', () => {
-//    console.log('onload')
-//    if (GBrowserIsCompatible()) {
-//        map = new GMap2(document.getElementById("map_canvas"));
-//        map.setCenter(new GLatLng(35.681379, 139.765577), 13);
-//
-//        directions = new GDirections(map, document.getElementById('route'));
-//    }
-//})
-//
-//$(window).on('unload', () => {
-//    console.log('unload')
-//    GUnload()
-//})
+$(function () {
+    //    initMap();
 
 
+});
 
-//function initMap() {
-//    console.log('initMap')
-//    var opts = {
-//        zoom: 15,
-//        center: new google.maps.LatLng(35.1239654, 136.9417741)
-//    };
-//    var map = new google.maps.Map(document.getElementById("map"), opts);
-//    $('#msg').text('堀田')
-//}
-//
-//function renderMap() {
-//    console.log('renderMap')
-//    var src = $('#src').text()
-//    var dst = $('#dst').text()
-//    var opts = {
-//        zoom: 15,
-//        center: new google.maps.LatLng(35.1253694, 136.9073667)
-//    };
-//    var map = new google.maps.Map(document.getElementById("map"), opts);
-//    $('#msg').text('熱田神宮')
-//}
-//
-//function dispRoute() {
-//    console.log('dispRoute')
-//    var from = $('#src').val()
-//    var to = $('#dst').val()
-//    console.log(from)
-//    console.log(to)
-//
-//    directions.clear();
-//
-//    str = 'from: ' + from + ' to: ' + to;
-//    directions.load(str, { locale: 'ja_JP' });
-//}
+$('#mapping').click(() => {
+    //    renderMap();
+    dispRoute();
+});
 
-window.onload = function (e) {
-    // init で初期化。基本情報を取得。
-    // https://developers.line.me/ja/reference/liff/#initialize-liff-app
-    liff.init(function (data) {
-        alert(data);
-        getProfile();
-        initializeApp(data);
-    });
-};
+$(window).on('load', () => {
+    console.log('onload')
+    if (GBrowserIsCompatible()) {
+        map = new GMap2(document.getElementById("map_canvas"));
+        map.setCenter(new GLatLng(35.681379, 139.765577), 13);
 
-// プロファイルの取得と表示
-function getProfile() {
-    // https://developers.line.me/ja/reference/liff/#liffgetprofile()
-    liff.getProfile().then(function (profile) {
-        document.getElementById('useridprofilefield').textContent = profile.userId;
-        document.getElementById('displaynamefield').textContent = profile.displayName;
+        directions = new GDirections(map, document.getElementById('route'));
+    }
+})
 
-        var profilePictureDiv = document.getElementById('profilepicturediv');
-        if (profilePictureDiv.firstElementChild) {
-            profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-        }
-        var img = document.createElement('img');
-        img.src = profile.pictureUrl;
-        img.alt = "Profile Picture";
-        img.width = 200;
-        profilePictureDiv.appendChild(img);
+$(window).on('unload', () => {
+    console.log('unload')
+    GUnload()
+})
 
-        document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-    }).catch(function (error) {
-        window.alert("Error getting profile: " + error);
-    });
+
+
+function initMap() {
+    console.log('initMap')
+    var opts = {
+        zoom: 15,
+        center: new google.maps.LatLng(35.1239654, 136.9417741)
+    };
+    var map = new google.maps.Map(document.getElementById("map"), opts);
+    $('#msg').text('堀田')
 }
 
-function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+function renderMap() {
+    console.log('renderMap')
+    var src = $('#src').text()
+    var dst = $('#dst').text()
+    var opts = {
+        zoom: 15,
+        center: new google.maps.LatLng(35.1253694, 136.9073667)
+    };
+    var map = new google.maps.Map(document.getElementById("map"), opts);
+    $('#msg').text('熱田神宮')
+}
+
+function dispRoute() {
+    console.log('dispRoute')
+    var from = $('#src').val()
+    var to = $('#dst').val()
+    console.log(from)
+    console.log(to)
+
+    directions.clear();
+
+    str = 'from: ' + from + ' to: ' + to;
+    directions.load(str, { locale: 'ja_JP' });
 }
