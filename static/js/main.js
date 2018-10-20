f66861db019161af8560ab3f6d8f4a930f048683
Takuro <fantasistakuro@gmail.com>
2018-10-21 03:55:12 +0900

    bug fix

--- static/js/main.js
+++ static/js/main.js
@@ -68,6 +68,7 @@ window.onload = function (e) {
     // init で初期化。基本情報を取得。
     // https://developers.line.me/ja/reference/liff/#initialize-liff-app
     liff.init(function (data) {
+        alert(data);
         getProfile();
         initializeApp(data);
     });
