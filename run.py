ea75f14902dfac13b31431014fb565b6c02d2c97
Takuro <fantasistakuro@gmail.com>
2018-10-27 03:56:32 +0900

    add waypoint

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
+from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify, send_file
 import numpy as np
 from linebot import (
     LineBotApi, WebhookHandler
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, MessageImagemapAction, ImagemapArea, BaseSize, ImagemapSendMessage
 )
 import os
 from os.path import join, dirname
@@ -15,6 +15,9 @@ from dotenv import load_dotenv
 import urllib.parse
 import urllib.request
 import xml.etree.ElementTree as ET
+import requests
+from io import BytesIO, StringIO
+from PIL import Image
 
 # 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
@@ -32,6 +35,8 @@ LIFF_URL = "開発中だよ"
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+src = None
+dst = None
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -58,27 +63,51 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=LIFF_URL))
 
+    if event.message.text == "地図":
+        # 座標取得
+        global src, dst
+        src_location = get_location(src)
+        dst_location = get_location(dst)
 
-# メッセージをランダムに表示するメソッド
-def picked_up():
-    messages = [
-        "こんにちは、あなたの名前を入力してください",
-        "やあ！お名前は何ですか？",
-        "あなたの名前を教えてね"
-    ]
-    # NumPy の random.choice で配列からランダムに取り出し
-    return np.random.choice(messages)
+        # Google Static Map API
+        map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?size=520x520&scale=2&maptype=roadmap&key={}'.format(
+            GOOGLE_MAPS_APIKEY)
+        map_image_url += '&markers=color:{}|label:{}|{},{}'.format('red', '', src_location[2], src_location[3])
+        map_image_url += '&markers=color:{}|label:{}|{},{}'.format('blue', '', dst_location[2], dst_location[3])
 
+        base_url = 'https://{}/imagemap/{}'.format(request.host, urllib.parse.quote_plus(map_image_url))
+
+        actions = [
+            MessageImagemapAction(
+                text="位置情報教えて!",
+                area=ImagemapArea(
+                    x=0,
+                    y=0,
+                    width=1040,
+                    height=1040
+                )
+            )
+        ]
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                ImagemapSendMessage(
+                    base_url=base_url,
+                    alt_text='地図地図',
+                    base_size=BaseSize(height=1024, width=1024),
+                    actions=actions,
+                ),
+                TextSendMessage(text='地図送ります')
+            ]
+        )
 
 # ここからウェブアプリケーション用のルーティングを記述
 # index にアクセスしたときの処理
 @app.route('/')
 def index():
-    title = "ようこそ"
-    message = picked_up()
     # index.html をレンダリングする
-    return render_template('index.html',
-                           message=message, title=title)
+    return render_template('index.html')
 
 
 # /post にアクセスしたときの処理
@@ -96,9 +125,22 @@ def post():
         return redirect(url_for('index'))
 
 
+@app.route('/imagemap/<path:url>/<size>')
+def imagemap(url, size):
+    map_image_url = urllib.parse.unquote(url)
+    response = requests.get(map_image_url)
+    img = Image.open(BytesIO(response.content))
+    img_resize = img.resize((int(size), int(size)))
+    byte_io = BytesIO()
+    img_resize.save(byte_io, 'PNG')
+    byte_io.seek(0)
+    return send_file(byte_io, mimetype='image/png')
+
+
 @app.route('/getimage', methods=['GET'])
 def getimage():
     # パラメータ取得
+    global src, dst
     src = request.args.get('src')
     dst = request.args.get('dst')
 
@@ -112,15 +154,34 @@ def getimage():
     map_image_url += '&markers=color:{}|label:{}|{},{}'.format('red', '', src_location[2], src_location[3])
     map_image_url += '&markers=color:{}|label:{}|{},{}'.format('blue', '', dst_location[2], dst_location[3])
 
+    base_url = 'https://{}/imagemap/{}'.format(request.host, urllib.parse.quote_plus(map_image_url))
+
     map_image_url_thumbnail = 'https://maps.googleapis.com/maps/api/staticmap?size=240x240&scale=2&maptype=roadmap&key={}'.format(
         GOOGLE_MAPS_APIKEY)
     map_image_url_thumbnail += '&markers=color:{}|label:{}|{},{}'.format('red', '', src_location[2], src_location[3])
     map_image_url_thumbnail += '&markers=color:{}|label:{}|{},{}'.format('blue', '', dst_location[2], dst_location[3])
 
+    actions = [
+        MessageImagemapAction(
+            text="位置情報教えて!",
+            area=ImagemapArea(
+                x=0,
+                y=0,
+                width=1040,
+                height=1040
+            )
+        )
+    ]
+
+    # baseSize = BaseSize(height=1040, width=1040)
+
     response = jsonify({
-        'result': 'hello world!',
+        'result': 'result',
         'map_image_url': urllib.parse.quote(map_image_url, safe='/:=?&'),
-        'map_image_url_thumbnail': urllib.parse.quote(map_image_url_thumbnail, safe='/:=?&')
+        'base_url': base_url,
+        'map_image_url_thumbnail': urllib.parse.quote(map_image_url_thumbnail, safe='/:=?&'),
+        # 'actions': actions,
+        # 'baseSize': baseSize
     })
 
     return make_response(response)
@@ -128,7 +189,8 @@ def getimage():
 
 def get_location(text):
     # 座標取得
-    geo_url = 'https://maps.googleapis.com/maps/api/place/textsearch/xml?query={}&key={}'.format(text, GOOGLE_MAPS_APIKEY)
+    geo_url = 'https://maps.googleapis.com/maps/api/place/textsearch/xml?query={}&key={}'.format(text,
+                                                                                                 GOOGLE_MAPS_APIKEY)
 
     geo_req = urllib.request.Request(geo_url)
     with urllib.request.urlopen(geo_req) as response:
@@ -147,5 +209,5 @@ def get_location(text):
 if __name__ == '__main__':
     app.debug = True  # デバッグモード有効化
     port = int(os.getenv('PORT', 3000))
-    app.run(host='0.0.0.0', port=port) # どこからでもアクセス可能に
+    app.run(host='0.0.0.0', port=port)  # どこからでもアクセス可能に
     # app.run()
